1e65bd3de22ce02cfa9c96658a0c6d864cc1ebea
Ramón Cigüenza <rcifuster@gmail>
2019-04-08 12:11:26 +0200

    Certificate & SSL Configuration fixed

--- src/main/java/com/msc/application/MicroCoreApplication.java
+++ src/main/java/com/msc/application/MicroCoreApplication.java
@@ -1,7 +1,5 @@
 package com.msc.application;
 
-import com.netflix.discovery.DiscoveryClient;
-import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,17 +7,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Bean;
-import java.security.NoSuchAlgorithmException;
 
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl.EurekaJerseyClientBuilder;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import com.msc.config.MicroCoreSwaggerConfiguration;
-
-import java.security.NoSuchAlgorithmException;
 
 
 /**
@@ -50,19 +43,4 @@ public class MicroCoreApplication {
 		logger.debug("-- Application API MicroCore Started --");
 	}
 
-    @Bean
-	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
-		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
-		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/client.p12");
-		System.setProperty("javax.net.ssl.keyStorePassword", "micro2019");
-		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/client.p12");
-		System.setProperty("javax.net.ssl.trustStorePassword", "micro2019");
-		EurekaJerseyClientImpl.EurekaJerseyClientBuilder builder = new EurekaJerseyClientBuilder();
-		builder.withClientName("client");
-		builder.withSystemSSLConfiguration();
-		builder.withMaxTotalConnections(10);
-		builder.withMaxConnectionsPerHost(10);
-		args.setEurekaJerseyClient(builder.build());
-		return args;
-	}
 }
